9bb183662c4016b64015286d721d42ed031c49e6
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-05-28 14:14:07 -0300

    feat(cart): Criado context para utilizar no carrinho de compras e adicionada api fake com json server

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,25 @@
+
+import { useContext } from "react"
 import { Link } from "react-router"
 import { FiShoppingCart } from "react-icons/fi"
 
+import { CartContext } from "../../contexts/CartContext"
+
 export function Header () {
+    const  { cartAmount }  = useContext(CartContext)
+
     return(
         <header className="w-full px-1 bg-slate-200">
             <nav className="w-full h-14 max-w-7xl flex items-center justify-between px-5 mx-auto" >
                 <Link to="/"  className="font-black text-2xl" > VendAqui!</Link>
-                <Link to="/cart" className="relative" > <FiShoppingCart size={24} color="#121212" /> <span className=" absolute -right-3 -top-3 px-2.5 bg-sky-600 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs" >2</span> </Link>
+                <Link to="/cart" className="relative" > 
+                    <FiShoppingCart size={24} color="#121212"/>
+                    { cartAmount > 0 && (
+                    <span className=" absolute -right-3 -top-3 px-2.5 bg-sky-600 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs" >
+                        {cartAmount}
+                    </span>
+                    )}
+                </Link>
             </nav>
         </header>
     )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,55 @@
+
+import { useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
+import { api } from "../../services/api"
+
+interface ProductProps{
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string
+}
 
 export function Home () {
+    const [products, setProducts] = useState<ProductProps[]>([]);
+
+useEffect(()=>{
+    async function getProducts(){
+        const response = await api.get("products")
+        setProducts(response.data)
+    }
+
+    getProducts();
+},[])
+
+
     return(
         <div>
             <main className=" w-full max-w-7xl px-4 mx-auto " >
                 <h1 className="font-bold text-2xl mb-4 mt-10 text-center" >Produtos em alta</h1>
 
                 <div className="  grid grid-cols-1 gap-6 sm:grid-cols-3 lg:grid-cols-5 " >
-                    <section className=" w-full  " >
+
+                    {products.map((product)=>(
+
+                    <section className=" w-full " key={product.id} >
                         <img 
-                        className="  rounded-lg max-h-50 mb-2 "
+                        className=" w-full rounded-lg max-h-72 mb-2 "
                         src="https://m.media-amazon.com/images/I/41uS5ZSX9yL.__AC_SX300_SY300_QL70_ML2_.jpg" 
-                        alt="foto do produto" 
+                        alt={product.title} 
                         />
 
-                        <p className=" font-medium mt-1 mb-2 " >S25 Ultra</p>
+                        <p className=" font-medium mt-1 mb-2 " >
+                            {product.title}
+                        </p>
 
                         <div className=" flex items-center gap-3 " >
                             <strong>
-                                R$ 8.149,00
+                                {product.price.toLocaleString("pt-br", {
+                                    style:"currency",
+                                    currency:"BRL"
+                                })}
                             </strong>
 
                             <button className="  bg-zinc-900 p-1 rounded " >
@@ -26,7 +57,9 @@ export function Home () {
                             </button>
                         </div>
 
-                    </section>
+                    </section> 
+                    ))}
+
                 </div>
             </main>
         </div>
